3a42a6f4f12fae8276b11c0fe3a80ce810cdbe77
minsukang <minsu910725@gmail.com>
2022-04-20 10:29:14 +0900

    Add:routerSetup

---
+++ src/components/Router.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+
+export default function Router() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <BrowserRouter>
+      <Routes>
+        {isLoggedIn ? (
+          <Route path="/" element={<Home />} />
+        ) : (
+          <Route path="/" element={<Auth />} />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+}
