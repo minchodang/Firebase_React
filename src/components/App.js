2999fa57eddbfa625691869d4eecce2c5addcc41
minsuKang <minsu910725@gmail.com>
2022-04-27 12:33:40 +0900

    Add:realtimeDb

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,12 @@ import { authService } from 'fBase';
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -19,7 +21,11 @@ function App() {
 
   return (
     <>
-      {init ? <Router isLoggedIn={isLoggedIn} /> : 'initializing'}
+      {init ? (
+        <Router isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        'initializing'
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,37 @@
 import { dbService } from 'fBase';
-import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+} from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 
-export default function Home() {
+export default function Home({ userObj }) {
   const [nweet, setNweet] = useState('');
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const q = query(collection(dbService, 'nweets'));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      const nweetObj = {
-        ...doc.data(),
-        id: doc.id,
-      };
-      setNweets((prev) => [nweetObj, ...prev]);
-    });
-  };
   useEffect(() => {
-    getNweets();
+    const q = query(
+      collection(dbService, 'nweets'),
+      orderBy('createdAt', 'desc')
+    );
+    onSnapshot(q, (snapshot) => {
+      const nweetArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArr);
+    });
   }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
       const docRef = await addDoc(collection(dbService, 'nweets'), {
-        nweet,
+        text: nweet,
         createdAt: Date.now(),
+        creatorId: userObj.uid,
       });
       console.log('Document written with ID: ', docRef.id);
     } catch (error) {
@@ -55,7 +61,7 @@ export default function Home() {
       <div>
         {nweets.map((nweet) => (
           <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
+            <h4>{nweet.text}</h4>
           </div>
         ))}
       </div>
