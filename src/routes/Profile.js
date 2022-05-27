ae1ee460e4a7774f439fa8f4af636da6bbded2b7
minsuKang <minsu910725@gmail.com>
2022-05-27 13:46:47 +0900

    add: getMyProfile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,30 @@
-import { authService } from 'fBase';
-import React from 'react';
+import { authService, dbService } from 'fBase';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, query, where } from '@firebase/firestore';
 
-export default function Profile() {
+export default function Profile({ userObj }) {
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate('/');
   };
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, 'nweets'),
+      where('creatorId', '==', userObj.uid)
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, ' => ', doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
